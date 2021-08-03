801452cf8e1be133178d6856ea77d975b88a482e
MenTal <87557914+TeAm-MenTal@users.noreply.github.com>
2021-08-03 21:33:29 +0530

    Create Cereal.py

---
+++ Cereal.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import random
+from datetime import datetime
+from os import execl
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
+from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12 , STRING13 , STRING14 , STRING15 ,STRING16 , STRING17 , STRING18 , STRING19 , STRING20 , STRING21 , STRING22 , STRING23 , STRING24 , STRING25 
+import asyncio
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from Utils import RAID, RRAID
+
+
+a = API_ID
+b = API_HASH
+smex = STRING
+smexx = STRING2
+smexxx = STRING3
+smexxxx = STRING4
+smexxxxx = STRING5
+sixth = STRING6
+seven = STRING7
+eight = STRING8
+ninth = STRING9
+tenth = STRING10
+eleve = STRING11
+twelv = STRING12
+thirt = STRING13
+forte = STRING14
+fifth = STRING15
+sieee = STRING16
+seeee = STRING17
+eieee = STRING18
+nieee = STRING19
+gandu = STRING20
+ekish = STRING21
+baish = STRING22
+teish = STRING23
+tfour = STRING24
+tfive = STRING25
+
+idk = ""
+ydk = ""
+wdk = ""
+sdk = ""
+hdk = ""
+adk = ""
+bdk = ""
+cdk = ""
+edk = ""
+ddk = ""
+vkk = ""
+kkk = ""
+lkk = ""
+mkk = ""
+sid = ""
+shy = ""
+aan = ""
+ake = ""
+eel = ""
+khu = ""
+shi = ""
+yaa = ""
+dav = ""
+raj = ""
+put = ""
+
+
+que = {}
+
+SMEX_USERS = []
+for x in SUDO: 
+    SMEX_USERS.append(x)
+    
+async def start_rizoel():
+    global idk
+    global ydk
+    global wdk
+    global sdk
+    global hdk
+    global adk
+    global bdk
+    global cdk
+    global ddk
+    global edk
+    global vkk
+    global kkk
+    global lkk
+    global mkk
+    global sid
+    global shy
+    global aan
+    global ake
+    global eel
+    global khu
+    global shi
+    global yaa
+    global dav
+    global raj
+    global put
