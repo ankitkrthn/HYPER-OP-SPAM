919300a51b21d837b127397ea768ebfd589133ce
MenTal <87557914+TeAm-MenTal@users.noreply.github.com>
2021-08-04 06:17:01 +0530

    Update Cereal.py

--- Cereal.py
+++ Cereal.py
@@ -1691,4 +1691,124 @@ async def _(e):
         else:
             await e.reply(usage, parse_mode=None, link_preview=None )
 
-                    
+        
+        
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@vkk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@kkk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@lkk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@mkk.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@sid.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@shy.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@aan.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@ake.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@eel.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@khu.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@shi.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@yaa.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@dav.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@raj.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+@put.on(events.NewMessage(incoming=True, pattern=r"\.spam"))
+async def spam(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
+    error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(rizoel) == 2:
+            message = str(rizoel[1])
+            counter = int(rizoel[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        elif e.reply_to_msg_id and smex.media:  
+            counter = int(rizoel[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            for _ in range(counter):
+                smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                await gifspam(e, smex)  
+        elif e.reply_to_msg_id and smex.text:
+            message = smex.text
+            counter = int(rizoel[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
+
+
+@idk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@ydk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@wdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@hdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@sdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@adk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@bdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@cdk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@edk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@ddk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@vkk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@kkk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@lkk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@mkk.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@sid.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@shy.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@aan.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@ake.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@eel.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@khu.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@shi.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@yaa.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@dav.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@raj.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+@put.on(events.NewMessage(incoming=True, pattern=r"\.delayspam"))
+
+async def spam(e):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗹𝗮𝘆𝗦𝗽𝗮𝗺\n\nCommand:\n\n.delayspam <sleep time> <count> <message to spam>\n\n.delayspam <sleep time> <count> <reply to a message>\n\nCount and Sleeptime must be a integer."
+    if e.sender_id in SMEX_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        smex = await e.get_reply_message()
+        rizoel = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+        rizoelop = rizoel[1:]
+        if len(rizoelop) == 2:
+            message = str(rizoelop[1])
+            counter = int(rizoelop[0])
+            sleeptime = float(rizoel[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "typing"):
+                    if e.reply_to_msg_id:
+                        await smex.reply(message)
+                    else:
+                        await e.client.send_message(e.chat_id, message)
+                    await asyncio.sleep(sleeptime)
+        elif e.reply_to_msg_id and smex.media:  
+            counter = int(rizoelop[0])
+            sleeptime = float(rizoel[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "document"):
+                    smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                    await gifspam(e, smex) 
+                await asyncio.sleep(sleeptime)
+        elif e.reply_to_msg_id and smex.text:
+            message = smex.text
+            counter = int(rizoelop[0])
+            sleeptime = float(rizoel[0])
+            for _ in range(counter):
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, message)
+                    await asyncio.sleep(sleeptime)
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None )
