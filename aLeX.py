4a4aaf7e61366826682032da7bd0f87d6378a8a9
TrAm aLeX <87557914+TeAm-MenTal@users.noreply.github.com>
2021-08-06 07:49:26 +0530

    Update aLeX.py

--- aLeX.py
+++ aLeX.py
@@ -1356,10 +1356,10 @@ async def _(e):
     if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
-        yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        alex = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
         if len(e.text) > 12:
-            message = str(yukki[0])
+            message = str(alex[0])
             a = await e.client.get_entity(message)
             g = a.id
             try:
